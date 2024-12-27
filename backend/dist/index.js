6c7c020fa1d20b0a6b9528b61728614aebf33bec
Avantika <avantika.197be22@chitkara,edu.in>
2024-12-27 20:57:02 +0530

    first commit

---
+++ backend/dist/index.js
@@ -0,0 +1,10 @@
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+//conntections and listeners
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+    app.listen(PORT, () => console.log("server started"));
+})
+    .catch((err) => console.log(err));
+//# sourceMappingURL=index.js.map
